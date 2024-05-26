64aa4a9cfef7f170c6fb00287d85b40752ef15a4
Username-ray <maedara96@craighead.school.nz>
2024-05-27 10:45:14 +1200

    Imported google fonts into the layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import '../app.scss'
+  @import url('https://fonts.googleapis.com/css2?family=Lusitana:wght@400;700&family=Playfair+Display+SC:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lusitana:wght@400;700&family=Playfair+Display+SC:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap');
+  
+  :global(h1) {
+    font-size: 96px;
+  }
 </script>
 
 <slot />
