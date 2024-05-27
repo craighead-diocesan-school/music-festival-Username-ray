51943fe842882a42bf8d2f61aef673c57f421c04
Username-ray <maedara96@craighead.school.nz>
2024-05-27 19:37:08 +1200

    Built main component of home page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -9,13 +9,13 @@
         </div>
       </nav>
       <div class="flexitem_header">
-        <div class="title-body">
+        <div class="title_body">
           <h1>Sunburst Festival</h1>
         </div>
       </div>
       <nav>
-        <div class="flexbox_nav">
-          <div class="flexitem_header">
+        <div class="flexitem_header">
+          <div class="flexbox_nav">
             <div class="flexitem_nav_2"><a href="/EnglishAccomodation">Accomodation</a></div>
             <div class="flexitem_nav_2"><a href="/MaoriHome">Eng - Maori</a></div>
           </div>
@@ -29,42 +29,52 @@
   body {
     font-family: "Playfair Display SC", serif;
     text-align: center;
+    background-color: #c4d3d4;
   }
   header {
     background-color: #a0e1fd;
     display: flex;
+    margin: 5px 5px 5px 5px;
   }
   .title {
     color: #fc9145;
     font-size: 96px;
-    padding: 30px 60px 20px 60px;
-    margin: 15px;
+    padding: 30px 5px 20px 5px;
+  }
+  h1 {
     filter: drop-shadow(4px 3px 3px #000000);
+    text-align: center;
+  }
+  .title_body {
+    min-width: 800px;
+    white-space: nowrap;
   }
   nav {
     font-size: 24px;
     filter: drop-shadow(0px 4px 4px #000000);
   }
   .flexbox_nav {
-    padding: 20px 25px 20px 25px;
     display: flex;
     flex-direction: column;
   }
   .flexitem_nav_1 {
     background: #fc9145;
-    color: #000000;
     margin: 10px;
-    padding: 10px;
+    padding: 15px 10px 15px 10px;
     transform: translate(0, 50%);
+    min-width: 225px;
+    border-radius: 30px;
   }
   .flexitem_nav_2 {
     background: #fc9145;
-    color: #000000;
-    margin: 10px;
-    padding: 10px;
-    transform: translate(0, -50%);
+    margin: 10px 10px 15px 10px;
+    padding: 15px 10px 15px 10px;
+    min-width: 225px;
+    border-radius: 30px;
   }
-
+  a {
+    color: #000000;
+  }
   .flexbox_header {
     display: flex;
     flex-direction: row;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,52 @@
 <Header />
 
 <main>
-  <h2>SvelteKit</h2>
+  <div class="container">
+    <img class="card-img" src="aditya-chinchure.jpg" alt="&copy; Aditya Chinchure" />
+  </div>
+  <div class="paragraph">
+    <p>Welcome to the ultimate music festival experience! Get ready for a day filled with live music, great food, and unforgettable memories.</p>
+    <p>Our festival features a diverse lineup of talented musicians from different genres, sure to satisfy every taste in music. With multiple stages set up throughout the venue, you can easily move from one act to the next, discovering new favorites and enjoying classic hits.</p>
+  </div>
+  <div class="container">
+    <img class="card-img" src="danny-howe-2.jpg" alt="&copy; Danny Howe" />
+  </div>
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+  <div class="paragraph">
+    <p>From up-and-coming artists to established headliners, we have curated an exciting lineup that will keep you dancing and singing along all day long. Our festival is the perfect opportunity to discover new music, see your favorite artists live, and make lasting memories with friends and family.</p>
+    <p>But the music is just the beginning. Our festival also boasts an impressive selection of food vendors, offering everything from classic festival fare to more gourmet options. You won't go hungry with the delicious treats and refreshing beverages available throughout the venue.</p>
+  </div>
+  <div class="container">
+    <img class="card-img" src="hanny-naibaho.jpg" alt="&copy; Hanny Naibaho" />
+  </div>
+
+  <div class="paragraph">
+    <p>In addition to the music and food, our festival also features a variety of activities to keep you entertained throughout the day. From art installations to merchandise booths, there is something for everyone to enjoy. You can even capture the memories with our festival photo booths and snap a selfie with your favorite artists.</p>
+    <p>So don't miss out on this one-of-a-kind music festival experience. Get your tickets now and join us for a day of music, fun, and community. We can't wait to see you there!</p>
+  </div>
 </main>
 
-<footer>
-  <p>&copy; Craighead Diocesan School 2024</p>
-</footer>
-
 <Footer />
+
+<style>
+  main {
+    font-family: "Lato", sans-serif;
+    background: #c4d3d4;
+    font-size: 40px;
+    line-height: 90px;
+    text-align: center;
+  }
+  .paragraph {
+    margin: 60px 40px;
+  }
+  .container {
+    background: #a0e1fd;
+    margin: 10px;
+    min-width: 1416px;
+    height: auto;
+  }
+  .card-img {
+    filter: drop-shadow(10px 10px 10px #000000);
+    padding: 15px 8px 0px 8px;
+  }
+</style>
