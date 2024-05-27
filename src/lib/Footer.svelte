547d8b7f087af08c97b8b8a7441a4b5583c179b1
Username-ray <maedara96@craighead.school.nz>
2024-05-27 20:08:47 +1200

    Created the footer of home page

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,14 @@
+<footer>
+  <p>&copy; Ray Maeda 2024</p>
+</footer>
+
+<style>
+  footer {
+    font-family: "Playfair Display SC", serif;
+    font-weight: bold;
+    font-size: 20px;
+    background: #a0e1fd;
+    padding: 50px;
+    margin-top: 60px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,28 +6,33 @@
 <Header />
 
 <main>
-  <div class="container">
+  <div class="container-img">
     <img class="card-img" src="aditya-chinchure.jpg" alt="&copy; Aditya Chinchure" />
   </div>
   <div class="paragraph">
     <p>Welcome to the ultimate music festival experience! Get ready for a day filled with live music, great food, and unforgettable memories.</p>
     <p>Our festival features a diverse lineup of talented musicians from different genres, sure to satisfy every taste in music. With multiple stages set up throughout the venue, you can easily move from one act to the next, discovering new favorites and enjoying classic hits.</p>
   </div>
-  <div class="container">
+  <div class="container-img">
     <img class="card-img" src="danny-howe-2.jpg" alt="&copy; Danny Howe" />
   </div>
-
   <div class="paragraph">
     <p>From up-and-coming artists to established headliners, we have curated an exciting lineup that will keep you dancing and singing along all day long. Our festival is the perfect opportunity to discover new music, see your favorite artists live, and make lasting memories with friends and family.</p>
     <p>But the music is just the beginning. Our festival also boasts an impressive selection of food vendors, offering everything from classic festival fare to more gourmet options. You won't go hungry with the delicious treats and refreshing beverages available throughout the venue.</p>
   </div>
-  <div class="container">
+  <div class="container-img">
     <img class="card-img" src="hanny-naibaho.jpg" alt="&copy; Hanny Naibaho" />
   </div>
-
   <div class="paragraph">
     <p>In addition to the music and food, our festival also features a variety of activities to keep you entertained throughout the day. From art installations to merchandise booths, there is something for everyone to enjoy. You can even capture the memories with our festival photo booths and snap a selfie with your favorite artists.</p>
     <p>So don't miss out on this one-of-a-kind music festival experience. Get your tickets now and join us for a day of music, fun, and community. We can't wait to see you there!</p>
+  </div>
+  <div class="container-ticket">
+    <p>Ticket</p>
+    <ul class="card-ticket">
+      <li>All tickets are $200 per person</li>
+      <li>Festival goers must be 15 years of age or older</li>
+    </ul>
   </div>
 </main>
 
@@ -35,16 +40,17 @@
 
 <style>
   main {
-    font-family: "Lato", sans-serif;
+    color: #000000;
     background: #c4d3d4;
     font-size: 40px;
     line-height: 90px;
     text-align: center;
   }
   .paragraph {
+    font-family: "Lato", sans-serif;
     margin: 60px 40px;
   }
-  .container {
+  .container-img {
     background: #a0e1fd;
     margin: 10px;
     min-width: 1416px;
@@ -53,5 +59,12 @@
   .card-img {
     filter: drop-shadow(10px 10px 10px #000000);
     padding: 15px 8px 0px 8px;
+  }
+  .container-ticket {
+    font-family: "Playfair Display SC", serif;
+    background: #fc9145;
+    filter: drop-shadow(10px 10px 10px #000000);
+    width: 700px;
+    margin: 0 auto;
   }
 </style>
